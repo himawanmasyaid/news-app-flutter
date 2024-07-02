d67b4bc00fc4405dfc5cbfdb3ddf6a5210c030ab
himawan.masyaid <himawan.masyaid@gmail.com>
2024-07-02 23:02:42 +0700

    create home to navigation

---
+++ lib/ui/screen/home/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+    );
+  }
+}
