068e7b683639affe3dbef4205093a19e2f577f44
himawan.masyaid <himawan.masyaid@gmail.com>
2024-07-02 23:13:51 +0700

    create logo in app bar

--- lib/ui/screen/home/home_screen.dart
+++ lib/ui/screen/home/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:news_app_flutter/data/constants.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+  static const routeName = '/home';
 
   @override
   State<HomeScreen> createState() => _MyWidgetState();
@@ -11,8 +13,16 @@ class _MyWidgetState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('Home'),
+        centerTitle: true,
+        title: Image.asset('${Asset.image}news_api_logo.png',
+            width: 70, height: 60),
+        elevation: 1.0,
+        backgroundColor: Colors.black,
+      ),
+      body: const Center(
+        child: Text('Hello, World!'),
       ),
     );
   }
