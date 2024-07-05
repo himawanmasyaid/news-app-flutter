c816b89f828fb8f7edd9d9cc093cdf7c96cc7e86
himawan.masyaid <himawan.masyaid@gmail.com>
2024-07-05 11:26:55 +0700

    update : news list view

--- lib/ui/screen/home/news_list_view.dart
+++ lib/ui/screen/home/news_list_view.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app_flutter/bloc/article/article_event.dart';
+import 'package:news_app_flutter/common/logger.dart';
 import 'package:news_app_flutter/data/model/article_model.dart';
+import 'package:news_app_flutter/bloc/article/article_bloc.dart';
+import 'package:news_app_flutter/bloc/article/article_state.dart';
 import 'package:news_app_flutter/ui/screen/home/news_item.dart';
 
 class NewsListView extends StatelessWidget {
-  final List<ArticleModel> articles;
-
-  const NewsListView({super.key, required this.articles});
+  const NewsListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: articles.length,
-      itemBuilder: (context, index) {
-        return ArticleItem(article: articles[index]);
+    context.read<ArticleBloc>().add(LoadArticlesEvent());
+
+    setLog("halo news list view");
+    return BlocBuilder<ArticleBloc, ArticleState>(
+      builder: (context, state) {
+        if (state.isLoading) {
+          setLog("loading state");
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          return ListView.builder(
+            itemCount: state.articles.length,
+            itemBuilder: (context, index) {
+              setLog("success state");
+              return ArticleItem(article: state.articles[index]);
+            },
+          );
+        }
       },
     );
   }
