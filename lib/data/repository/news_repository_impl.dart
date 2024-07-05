158a7674c916bdbd7eb947ceac450db94c8e7cfb
himawan.masyaid <himawan.masyaid@gmail.com>
2024-07-05 11:26:00 +0700

    create news repository

--- lib/data/repository/news_repository_impl.dart
+++ lib/data/repository/news_repository_impl.dart
@@ -4,37 +4,64 @@ import 'package:news_app_flutter/domain/repository/news_repository.dart';
 import 'package:news_app_flutter/network/network_provider.dart';
 
 class NewsRepositoryImpl implements NewsRepository {
-  final NetworkProvider _networkProvider;
-  final NewsRemoteDataSource _dataSource;
+  // final NetworkProvider _networkProvider;
+  // final NewsRemoteDataSource _dataSource;
 
-  NewsRepositoryImpl(this._networkProvider, this._dataSource);
+  // NewsRepositoryImpl(this._networkProvider, this._dataSource);
 
-  @override
-  Future<List<ArticleModel>> fetchNews() async {
-    final response = await _networkProvider.fetchTopHeadlines();
-    return response['articles']
-        .map((json) => ArticleModel.fromJson(json))
-        .toList();
-  }
+  // @override
+  // Future<List<ArticleModel>> fetchNews() async {
+  //   final response = await _networkProvider.fetchTopHeadlines();
+  //   return response['articles']
+  //       .map((json) => ArticleModel.fromJson(json))
+  //       .toList();
+  // }
 
-  @override
-  Future<List<ArticleModel>> fetchNewsByCategory(String category) async {
-    final response = await _networkProvider.fetchNewsByCategory(category);
-    return response['articles']
-        .map((json) => ArticleModel.fromJson(json))
-        .toList();
-  }
+  // @override
+  // Future<List<ArticleModel>> fetchNewsByCategory(String category) async {
+  //   final response = await _networkProvider.fetchNewsByCategory(category);
+  //   return response['articles']
+  //       .map((json) => ArticleModel.fromJson(json))
+  //       .toList();
+  // }
 
-  @override
-  Future<List<ArticleModel>> fetchNewsByCountry(String country) async {
-    final response = await _networkProvider.fetchNewsByCountry(country);
-    return response['articles']
-        .map((json) => ArticleModel.fromJson(json))
-        .toList();
-  }
+  // @override
+  // Future<List<ArticleModel>> fetchNewsByCountry(String country) async {
+  //   final response = await _networkProvider.fetchNewsByCountry(country);
+  //   return response['articles']
+  //       .map((json) => ArticleModel.fromJson(json))
+  //       .toList();
+  // }
 
   @override
   Future<List<ArticleModel>> getNewsSample() {
-    return _dataSource.getNewsSample();
+    throw UnimplementedError();
+    // return _dataSource.getNewsSample();
+  }
+
+  @override
+  Future<List<ArticleModel>> getArticles() async {
+    await Future.delayed(const Duration(seconds: 2));
+    return [
+      ArticleModel(
+        author: 'Author 1',
+        title: 'Title 1',
+        description: 'Description 1',
+        url: 'https://example.com/article1',
+        urlToImage: 'https://example.com/image1',
+        publishedAt: '2022-01-01',
+        content: 'Content 1',
+      ),
+      ArticleModel(
+        author: 'Author 2',
+        title: 'Title 2',
+        description: 'Description 2',
+        url: 'https://example.com/article2',
+        urlToImage: 'https://example.com/image2',
+        publishedAt: '2022-01-02',
+        content: 'Content 2',
+      ),
+      // Add more articles here
+    ];
   }
 }
--- lib/domain/repository/news_repository.dart
+++ lib/domain/repository/news_repository.dart
@@ -1,8 +1,6 @@
 import 'package:news_app_flutter/data/model/article_model.dart';
 
 abstract class NewsRepository {
-  Future<List<ArticleModel>> fetchNews();
-  Future<List<ArticleModel>> fetchNewsByCategory(String category);
-  Future<List<ArticleModel>> fetchNewsByCountry(String country);
   Future<List<ArticleModel>> getNewsSample();
+  Future<List<ArticleModel>> getArticles();
 }
